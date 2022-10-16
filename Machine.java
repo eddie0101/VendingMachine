b1cb8b50c90589bd1c76f9d4c53f7fc4931432c0
Eduard <ghiuzaneduard@gmail.com>
2022-10-16 11:12:05 +0300

    fixed machine constructor and added machine toString

--- Machine.java
+++ Machine.java
@@ -2,11 +2,21 @@ public class Machine {
     private Item[][] items;
 
     public Machine(Item[][] items) {
+        this.items = new Item[items.length][items[0].length];
         for (int i = 0; i < items.length; i++) {
             for (int j = 0; j < items[i].length; j++) {
                 this.items[i][j] = new Item(items[i][j]);
             }
         }
+    }
+
+    public String toString() {
+        String temp = "The machine has the following items inside:\n";
+
+        for (int i = 0; i < items[i].length; i++) {
+            temp += items[i].toString() + "\n";
+        }
+        return temp;
     }
 
     public Item getItem(int row, int spot) {
