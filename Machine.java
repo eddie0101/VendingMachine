337008cee836ef013460cd941c8df12910fa5900
Eduard <ghiuzaneduard@gmail.com>
2022-10-16 11:00:18 +0300

    initial classes and main preparations

--- Machine.java
+++ Machine.java
@@ -1,3 +1,15 @@
 public class Machine {
-    
+    private Item[][] items;
+
+    public Machine(Item[][] items) {
+        for (int i = 0; i < items.length; i++) {
+            for (int j = 0; j < items[i].length; j++) {
+                this.items[i][j] = new Item(items[i][j]);
+            }
+        }
+    }
+
+    public Item getItem(int row, int spot) {
+        return items[row - 1][spot - 1];
+    }
 }
