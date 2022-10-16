0a0b9cca76a0ecf4cfa45710239c17f32deb606b
Eduard <ghiuzaneduard@gmail.com>
2022-10-16 13:29:21 +0300

    made it interractive

--- Main.java
+++ Main.java
@@ -1,19 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("\t************************************************");
-        System.out.println("\t             WELCOME TO JAVA DRINKS!            ");
-        System.out.println("\t************************************************");
+        System.out.print("\033[H\033[2J");
 
-        Item[][] items = new Item[][] {
-            { new Item("Pepsi", 1.99, 3), new Item("Fresca", 1.49, 3), new Item("Brisk", 2.49, 2) },
-            { new Item("Fanta", 1.99, 2), new Item("Barq's", 1.49, 2), new Item("A & W", 2.49, 3) },
-            { new Item("Crush", 1.99, 2), new Item("C-Cola", 1.49, 2), new Item("Berry", 2.49, 1) }
-        };
+        System.out.println("\t****************************************************");
+        System.out.println("\t               WELCOME TO JAVA DRINKS!              ");
+        //System.out.println("\t****************************************************");
 
-        Machine machine = new Machine(items);
+        Machine machine = new Machine(
+            new Item[][] {
+                { new Item("Pepsi", 1.99, 3), new Item("Fresca", 1.49, 3), new Item("Brisk", 2.49, 2) },
+                { new Item("Fanta", 1.99, 2), new Item("Barq's", 1.49, 2), new Item("A & W", 2.49, 3) },
+                { new Item("Crush", 1.99, 2), new Item("C-Cola", 1.49, 2), new Item("Berry", 2.49, 1) }
+            }
+        );
+        System.out.println(machine);
 
-        System.out.println(machine.getItem(3, 3));
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Pick a row: ");
+            int row = scan.nextInt();
+            System.out.print("Pick a spot: ");
+            int spot = scan.nextInt();
+    
+            if (machine.dispense(row, spot) == true) {
+                System.out.print("Enjoy your drink! Press 1 to get another: ");
+            }
+            else {
+                System.out.print("Sorry, we're out of this item. Press 1 to purchase another: ");
+            }
+            int input = scan.nextInt();
+            if (input == 1) {
+                System.out.println(machine);
+            }
+            else {
+                System.exit(0);
+            }
+
+        }
+
     }
 
 }
